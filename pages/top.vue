112ef5a176b6b933c336ddc293c4dae8b3b4160e
Akira Shinohara <k017c1067@it-neec.jp>
2020-02-01 17:07:09 +0000

    [feat] relative top content position

---
+++ pages/top.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="top">
+    <b-container>
+      <b-jumbotron
+        header="BootstrapVue"
+        lead="Bootstrap v4 Components for Vue.js 2"
+      >
+        <p>For more information visit our website</p>
+        <b-btn variant="primary" href="https://bootstrap-vue.js.org/"
+          >More Info</b-btn
+        >
+      </b-jumbotron>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style></style>
