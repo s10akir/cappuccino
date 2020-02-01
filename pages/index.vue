cea50aa21616b6d642507906f393e84aba10980f
Akira Shinohara <k017c1067@it-neec.jp>
2020-02-01 16:36:02 +0000

    [feat] change index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,31 @@
 <template>
-  <div class="container">
-    <div>
-      <b-img-lazy
-        id="chara"
-        class="fixed-bottom"
-        src="~/assets/images/top_chara.png"
-        fluid
-      />
-      <h1 class="title">
-        cappuccino
-      </h1>
-      <h2 class="subtitle">
-        NO COFFEE NO LIFE.
-      </h2>
-      <div class="links">
-        <b-button
-          v-if="!$store.state.authUser"
-          variant="outline-success"
-          href="/auth/slack"
-          size="lg"
-        >
-          Login
-        </b-button>
-        <b-button
-          v-if="$store.state.authUser"
-          variant="outline-danger"
-          href="/auth/logout"
-          size="lg"
-        >
-          Logout
-        </b-button>
-      </div>
-    </div>
+  <div id="index" class="app">
+    <b-container class="justify-content-center text-center">
+      <h1 class="title">cappuccino</h1>
+      <h2 class="subtitle">NO COFFEE NO LIFE.</h2>
+      <b-button
+        v-if="!$store.state.authUser"
+        variant="outline-success"
+        href="/auth/slack"
+        size="lg"
+      >
+        Login
+      </b-button>
+      <b-button
+        v-if="$store.state.authUser"
+        variant="outline-danger"
+        href="/auth/logout"
+        size="lg"
+      >
+        Logout
+      </b-button>
+    </b-container>
+    <b-img-lazy
+      id="chara"
+      class="fixed-bottom"
+      src="~/assets/images/top_chara.png"
+      fluid
+    />
   </div>
 </template>
 
@@ -42,35 +36,20 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 
 #chara {
