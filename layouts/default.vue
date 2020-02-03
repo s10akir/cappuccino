60921ddda460140b2065c62978193e7295b5acf5
Akira Shinohara <k017c1067@it-neec.jp>
2020-02-03 15:46:01 +0000

    [clean]

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-    <nuxt />
+    <transition name="page">
+      <nuxt />
+    </transition>
   </div>
 </template>
 
 <style>
 .app {
   margin-top: 80px;
+}
+
+/* page trandition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+
+/* TODO: CSS切り出し */
+.center-vertical {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,6 @@
     <b-container class="justify-content-center text-center">
       <h1 class="title">cappuccino</h1>
       <h2 class="subtitle">NO COFFEE NO LIFE.</h2>
-      <p v-if="$store.state.auth.user">
-        Welcome back, {{ $store.state.auth.user.name }} !
-      </p>
       <b-button
         v-if="!$store.state.auth.user"
         @click="login"
@@ -14,16 +11,20 @@
       >
         Login
       </b-button>
-      <b-button
-        v-if="$store.state.auth.user"
-        @click="logout"
-        variant="outline-danger"
-        size="lg"
-      >
-        Logout
-      </b-button>
+      <div v-if="$store.state.auth.user" class="d-inline-flex flex-column">
+        <p>Welcome back, {{ $store.state.auth.user.name }} !</p>
+        <b-button
+          v-if="$store.state.auth.user"
+          @click="logout"
+          class="m-1"
+          variant="outline-danger"
+          size="lg"
+        >
+          Logout
+        </b-button>
+      </div>
     </b-container>
-    <b-img-lazy
+    <b-img
       id="chara"
       class="fixed-bottom"
       src="~/assets/images/top_chara.png"
@@ -61,30 +62,6 @@ export default {
 </script>
 
 <style>
-.center-vertical {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
 #chara {
   z-index: -1;
 }
